ed6b0041b05779baa0a48629b3ddb2655b5c3b71
Emil Margrain <73889326+dog-ball@users.noreply.github.com>
2022-04-19 16:17:02 +0100

    Use simply market_wrapper instead of best_matching_algorithm
    This requires the changing of the names in the market maker

--- gsy_framework/matching_algorithms/__init__.py
+++ gsy_framework/matching_algorithms/__init__.py
@@ -9,4 +9,5 @@ __all__ = [
 from .abstract_matching_algorithm import BaseMatchingAlgorithm
 from .pay_as_bid_matching_algorithm import PayAsBidMatchingAlgorithm
 from .pay_as_clear_matching_algorithm import PayAsClearMatchingAlgorithm
-from .best_matching_algorithm import BestPayAsBidMatchingAlgorithm, BestPayAsClearMatchingAlgorithm, BestClusterPayAsClearMatchingAlgorithm
+from simply.market_wrapper import BestPayAsBidMatchingAlgorithm, BestPayAsClearMatchingAlgorithm, \
+    BestClusterPayAsClearMatchingAlgorithm
